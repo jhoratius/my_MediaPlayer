9a2046d5fcae2ebf1a66574964d91fa43619c45d
Julien Horatius <julien.horatius@gmail.com>
2021-02-03 12:02:09 +0100

    MyMediaPlayer

--- assets/JS/media.js
+++ assets/JS/media.js
@@ -1,7 +1,16 @@
-var video = document.querySelector('.video');
-var juice = document.querySelector('.orange_bar');
-var btn = document.getElementById('play_pause');
+    // Var Initialization 
+    var video = document.querySelector('video');
+    var juice = document.querySelector('orange_bar');
+    var btn = document.getElementById('play_pause');
+    var backwardBtn = document.getElementsByClassName('backward')[0];
+    var forwardBtn = document.getElementsByClassName('forward')[0];
+    var currentVolume = document.getElementById("currentVolume");
 
+    // Event Initialization
+    currentVolume.addEventListener("change", setvolume, false);
+    
+
+// Play Pause
 function togglePlayPause(){
     if(video.paused){
         btn.className = "pause";
@@ -22,10 +31,20 @@ video.addEventListener('timeupdate', function(){
         btn.className = 'play';
     }
 })
-var currentVolume;
-currentVolume = document.getElementById("currentVolume");
-currentVolume.addEventListener("change", setvolume, false)
+backwardBtn.addEventListener("click", fwd, false);
+forwardBtn.addEventListener("click", bwd, false);
+// Skip Forward / Skip Backward 
+function fwd(){
+    video.currentTime -= 3;
+}
+function bwd(){
+    video.currentTime += 3;
+    if(video.currentTime >= video.duration){
+        forwardBtn.preventDefault(forwardBtn.onclick);
+    }
+}
 
+// Volume
 function setvolume(){
     video.volume = currentVolume.value/100;
 }
